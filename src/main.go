8f3550ac43da6a1d68c696b2eb72a9833c96f494
Adam <davidatomhernandez@gmail.com>
2017-10-26 15:38:24 -0400

    start at the beginning

---
+++ src/main.go
@@ -0,0 +1,74 @@
+package main
+
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+// Oh dear. Globals...
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+// configure upgrader
+var upgrader = websocket.Upgrader{}
+// ...well.
+
+type Message struct {
+	Email string `json:"email"`
+	Username string `json:"username"`
+	Message string `json:"message"`
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// ensure connection close when function returns
+	defer ws.Close()
+	clients[ws] = true
+
+	for {
+		var msg Message
+		// Read in a new message as JSON and map it to a Message object
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+		// send the new message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// grab next message from the broadcast channel
+		msg := <- broadcast
+		// send it out to every client that is currently connected
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("../public")
+	http.handle("/", fs)
+	http.HandleFunc("/ws", handleConnections)
+	go handleMessages()
+
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListAndServe: ", err)
+	}
+}
